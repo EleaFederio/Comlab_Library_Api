c75faa4bcea267d5bd534685811020ea530911dc
Eleazar Federio <eleazarfederio@rocketmail.com>
2019-05-25 23:25:53 +0800

    single fetch

--- api/index.php
+++ api/index.php
@@ -1,9 +1,21 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include '../model/Posts.php';
 $p_model = new Posts();
+
 
 // conditions
 if(isset($_GET['posts'])){
     echo $p_model->getPost();
 }
+
+if(isset($_GET['id'])){
+    echo $p_model->getSingle($_GET['id']);
+}
+
+//Get POST data
+$data = json_decode(file_get_contents("php://input"));
--- model/Posts.php
+++ model/Posts.php
@@ -30,6 +30,34 @@ class Posts{
         }
     }
 
+    function getSingle($id){
+        $post_arr = array();
+        $post_arr['data'] = array();
+
+    $query = "SELECT * FROM `posts` JOIN `categories` ON `posts`.`category_id` = `categories`.`id` WHERE `posts`.`id` = {$id} ";
+    echo $query;    
+    if($result = $this->db->connect()->query($query)){
+            while($row = $result->fetch_object()){
+                $post_item = array(
+                    'id' => $row->id,
+                    'title' => $row->title,
+                    'body' => $row->body,
+                    'author' => $row->author,
+                    'category_id' => $row->category_id,
+                    'category_name' => $row->name
+                );
+                array_push($post_arr['data'], $post_item);
+            }
+            // echo '<pre>';
+            return json_encode($post_arr);
+            // echo '</pre>';
+        }
+    }
+
+    function postsCreate($user, $title, $body, $category){
+        
+    }
+
 }
 
 $post = new Posts();
